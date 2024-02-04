96cb9b3e0c42342cd1f898205b62c883afdc2527
CodeSandbox <support@codesandbox.io>
2024-02-04 16:08:25 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,83 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+const phrases = [
+  "Nein",
+  "Are you sure?",
+  "Really sure😢",
+  "I'll be big sad😕",
+  "Babes Please🥺",
+  "Don't do this to me🫤",
+  "I'm criiiii😭",
+  "Jy het my hart gebreek😭💔",
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  function handleYesClick() {
+    setYesPressed(true);
+  }
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="valentine-container">
+      {yesPressed ? (
+        <>
+          <img
+            src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
+            alt="bear-Kissing"
+          />
+          <div className="text">
+            Chantel. Each time I look at you, a constellation of moments happen.
+            And when you blink, its like watching shooting stars your gaze holds
+            the mystery of a blackhole where time and realty are lost.
+          </div>
+          <div className="text"> </div>
+
+          <div className="text">
+            You make me want to be so much better for you. I actually want to
+            try for you. You make me want to give you the world. Not because I
+            want to do that. Because you actually deserve it. Thats how amazing
+            you are. So please allow me the grace of being your Valentines day
+            parter. This year and every year after this. I promise to be the
+            best partner and Valentine you have ever had and will ever have
+          </div>
+        </>
+      ) : (
+        <>
+          <img
+            className="kiss"
+            src="https://media1.tenor.com/m/al4a1pG1f8YAAAAC/jump-bear.gif"
+            alt="bear with heart"
+          />
+          <div className="text">Chantel, wärst du mein Valentinsgruß?</div>
+          <div className="both-Button">
+            <button
+              className="yesButton"
+              style={{
+                fontSize: yesButtonSize,
+                backgroundColor: "rgb(248, 229, 89)",
+              }}
+              onClick={handleYesClick}
+            >
+              Ja
+            </button>
+            <button className="NoButton" style={{}} onClick={handleNoClick}>
+              {getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
